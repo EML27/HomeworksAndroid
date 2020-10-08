61f7ff369c89922852556c9a07bb6b8bf1b5b3d2
EML27 <49094536+EML27@users.noreply.github.com>
2020-10-08 19:27:30 +0300

    google pay done

--- app/src/main/java/com/example/homeworksandroid/MainActivity.kt
+++ app/src/main/java/com/example/homeworksandroid/MainActivity.kt
@@ -1,11 +1,175 @@
 package com.example.homeworksandroid
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.wallet.*
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONException
+import org.json.JSONObject
+import kotlin.math.roundToLong
+
+
+class MainActivity : Activity() {
+
+    companion object {
+        private const val LOAD_PAYMENT_DATA_REQUEST_CODE = 991
+        private const val shippingCost = 0.001.toLong()
+    }
+
+    private lateinit var paymentsClient: PaymentsClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize a Google Pay API client for an environment suitable for testing.
+        // It's recommended to create the PaymentsClient object inside of the onCreate method.
+        paymentsClient = PaymentsUtil.createPaymentsClient(this)
+        possiblyShowGooglePayButton()
+
+        googlePayButton.setOnClickListener { requestPayment() }
+    }
+
+    private fun possiblyShowGooglePayButton() {
+
+        val isReadyToPayJson = PaymentsUtil.isReadyToPayRequest() ?: return
+        val request = IsReadyToPayRequest.fromJson(isReadyToPayJson.toString()) ?: return
+
+        // The call to isReadyToPay is asynchronous and returns a Task. We need to provide an
+        // OnCompleteListener to be triggered when the result of the call is known.
+        val task = paymentsClient.isReadyToPay(request)
+        task.addOnCompleteListener { completedTask ->
+            try {
+                completedTask.getResult(ApiException::class.java)?.let(::setGooglePayAvailable)
+            } catch (exception: ApiException) {
+                // Process error
+                Log.w("isReadyToPay failed", exception)
+            }
+        }
+    }
+
+
+    private fun setGooglePayAvailable(available: Boolean) {
+        if (available) {
+            googlePayButton.visibility = View.VISIBLE
+        } else {
+            Toast.makeText(
+                this,
+                "Unfortunately, Google Pay is not available on this device",
+                Toast.LENGTH_LONG
+            ).show();
+        }
+    }
+
+    private fun requestPayment() {
+
+        // Disables the button to prevent multiple clicks.
+        googlePayButton.isClickable = false
+
+        // The price provided to the API should include taxes and shipping.
+        // This price is not displayed to the user.
+        val garmentPriceMicros = (0.000001 * 1).roundToLong()
+        val price = (garmentPriceMicros + shippingCost).microsToString()
+
+        val paymentDataRequestJson = PaymentsUtil.getPaymentDataRequest(price)
+        if (paymentDataRequestJson == null) {
+            Log.e("RequestPayment", "Can't fetch payment data request")
+            return
+        }
+        val request = PaymentDataRequest.fromJson(paymentDataRequestJson.toString())
+
+        // Since loadPaymentData may show the UI asking the user to select a payment method, we use
+        // AutoResolveHelper to wait for the user interacting with it. Once completed,
+        // onActivityResult will be called with the result.
+        if (request != null) {
+            AutoResolveHelper.resolveTask(
+                paymentsClient.loadPaymentData(request), this, LOAD_PAYMENT_DATA_REQUEST_CODE
+            )
+        }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.e("requestCode", requestCode.toString())
+        Log.e("resultCode", resultCode.toString())
+        when (requestCode) {
+            // value passed in AutoResolveHelper
+            LOAD_PAYMENT_DATA_REQUEST_CODE -> {
+                when (resultCode) {
+                    Activity.RESULT_OK ->
+                        data?.let { intent ->
+                            val paymentData =
+                                PaymentData.getFromIntent(intent)?.let(::handlePaymentSuccess)
+                            Log.e("Payment_DATA", paymentData.toString())
+                        }
+                    Activity.RESULT_CANCELED -> {
+                        // Nothing to do here normally - the user simply cancelled without selecting a
+                        // payment method.
+                    }
+
+                    AutoResolveHelper.RESULT_ERROR -> {
+                        AutoResolveHelper.getStatusFromIntent(data)?.let {
+                            handleError(it.statusCode)
+                        }
+                    }
+                }
+                // Re-enables the Google Pay payment button.
+                googlePayButton.isClickable = true
+            }
+        }
+    }
+
+    private fun handlePaymentSuccess(paymentData: PaymentData) {
+        val paymentInformation = paymentData.toJson() ?: return
+
+        try {
+            // Token will be null if PaymentDataRequest was not constructed using fromJson(String).
+            val paymentMethodData =
+                JSONObject(paymentInformation).getJSONObject("paymentMethodData")
+
+            // If the gateway is set to "example", no payment information is returned - instead, the
+            // token will only consist of "examplePaymentMethodToken".
+            if (paymentMethodData
+                    .getJSONObject("tokenizationData")
+                    .getString("type") == "PAYMENT_GATEWAY" && paymentMethodData
+                    .getJSONObject("tokenizationData")
+                    .getString("token") == "examplePaymentMethodToken"
+            ) {
+                buildAlertDialog(
+                    "Warning", "Gateway name set to \"example\" - please modify " +
+                            "Constants.java and replace it with your own gateway."
+                )
+            }
+
+            val billingName = paymentMethodData.getJSONObject("info")
+                .getJSONObject("billingAddress").getString("name")
+
+            val token = paymentMethodData
+                .getJSONObject("tokenizationData")
+                .getString("token")
+
+            buildAlertDialog(message = "message in alert dialog")
+        } catch (e: JSONException) {
+            Log.e("handlePaymentSuccess", "Error: " + e.toString())
+        }
+    }
+
+    private fun handleError(statusCode: Int) {
+        Log.w("loadPaymentData failed", String.format("Error code: %d", statusCode))
+    }
+
+    private fun buildAlertDialog(title: String = "", message: String) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Ыыыыыыы", null)
+            .create()
+            .show()
     }
 }
