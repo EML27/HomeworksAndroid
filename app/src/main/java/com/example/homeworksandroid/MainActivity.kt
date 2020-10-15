509c396dee317359a97b794da0a23d61652b1f20
EML27 <49094536+EML27@users.noreply.github.com>
2020-10-15 19:27:30 +0300

    first commit

--- app/src/main/java/com/example/homeworksandroid/MainActivity.kt
+++ app/src/main/java/com/example/homeworksandroid/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.homeworksandroid
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnShowHide.setOnClickListener { changeHiddenButtonVisibility() }
+    }
+
+    private fun changeHiddenButtonVisibility() {
+        if (btnHidden.isVisible) {
+            btnHidden.visibility = View.GONE
+        } else {
+            btnHidden.visibility = View.VISIBLE
+        }
     }
 }
